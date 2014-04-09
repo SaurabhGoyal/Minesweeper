f9eda96784149a480db8d3b752f08b79556d44b3
Saurabh Goyal <saurabh.2561@gmail.com>
2014-04-09 17:56:04 +0530

    Update Game.xaml.cs

--- Game.xaml.cs
+++ Game.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Threading;
 
 
 namespace MineSweeper
@@ -16,16 +17,18 @@ namespace MineSweeper
     public partial class Game : Microsoft.Phone.Controls.PhoneApplicationPage
     {
         int gridSize;
+        Boolean gameOn=false;
+        Boolean flagOn=false;
         Random rand;
         Button[,] buttons;
-        Boolean[,] opened;
+        Boolean[,] opened,flagged;
         int numOfMines,numOfOpened=0;
         Button[] mineButtons;
+        int minutes = 0, seconds = 0;
         public Game()
         {
             InitializeComponent();
             rand = new Random();
-            
         }
         protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
         {
@@ -38,9 +41,13 @@ namespace MineSweeper
             mineButtons = new Button[numOfMines];
             buttons = new Button[gridSize, gridSize];
             opened = new Boolean[gridSize, gridSize];
+            flagged = new Boolean[gridSize, gridSize];
             generateButtons();
             deployMines();
             fillBoard();
+            
+            Thread th=new Thread(new ThreadStart(this.manageTimer));
+            th.Start();
         }
         private void generateButtons()
         {
@@ -67,17 +74,25 @@ namespace MineSweeper
                 }
             }
         }
+
         private void cell_Click(object sender, RoutedEventArgs e)
         {
             Button current=(Button)sender;
-            
             for (int i = 0; i < gridSize; i++)
             {
                 for (int j = 0; j < gridSize; j++)
                 {
                     if (buttons[i, j].Equals(current))
                     {
-                        if (buttons[i,j].Content.Equals("*"))
+                        if (flagOn) 
+                        {
+                            ImageBrush background = new ImageBrush();
+                            background.ImageSource = new System.Windows.Media.Imaging.BitmapImage(new Uri("flagImage.jpg", UriKind.Relative));
+                            if (flagged[i, j]) { current.Background = new SolidColorBrush(Color.FromArgb(0xff, 0x0c, 0x1b, 0x27)); flagged[i, j] = false; }
+                            else { current.Background = background; flagged[i, j] = true; }
+                            return;
+                        } 
+                        else if (buttons[i, j].Content.Equals("*"))
                         {
                             overGame();
                             return;
@@ -90,32 +105,36 @@ namespace MineSweeper
         }
         private void revealCells(int i,int j)
         {
-            String s=(String)(buttons[i,j].Content);
-            buttons[i, j].Foreground = new SolidColorBrush(Colors.Black);
-            buttons[i, j].Background = new SolidColorBrush(Color.FromArgb(0xff,0x4b,0xa0,0xe6)); 
-            opened[i, j] = true;
-            numOfOpened++;
-            buttons[i, j].IsHitTestVisible = false;
-            if (numOfOpened == (gridSize * gridSize)-numOfMines)
-                winGame();
-            if (s.Equals("0"))
-            {
-                buttons[i, j].Foreground = new SolidColorBrush(Colors.Transparent);
-                if ((i >= 0 && i < gridSize) && (j >= 0 && j < gridSize))
-                {
-                    if (((i - 1) >= 0) && (!opened[i - 1, j]))
-                        revealCells(i - 1, j);
-                     if (((i + 1) <= gridSize-1) && (!opened[i + 1, j]))
-                        revealCells(i + 1, j);
-                     if (((j - 1) >= 0) && (!opened[i, j-1]))
-                        revealCells(i, j - 1);
-                     if (((j + 1) <=gridSize-1) && (!opened[i, j+1]))
-                        revealCells(i, j + 1);
-                }           
-            }
+                if(!gameOn)
+                    gameOn = true;
+                
+                    String s = (String)(buttons[i, j].Content);
+                    buttons[i, j].Foreground = new SolidColorBrush(Colors.Black);
+                    buttons[i, j].Background = new SolidColorBrush(Color.FromArgb(0xff, 0x4b, 0xa0, 0xe6));
+                    opened[i, j] = true;
+                    numOfOpened++;
+                    buttons[i, j].IsHitTestVisible = false;
+                    if (numOfOpened == (gridSize * gridSize) - numOfMines)
+                        winGame();
+                    if (s.Equals("0"))
+                    {
+                        buttons[i, j].Foreground = new SolidColorBrush(Colors.Transparent);
+                        if ((i >= 0 && i < gridSize) && (j >= 0 && j < gridSize))
+                        {
+                            if (((i - 1) >= 0) && (!opened[i - 1, j]))
+                                revealCells(i - 1, j);
+                            if (((i + 1) <= gridSize - 1) && (!opened[i + 1, j]))
+                                revealCells(i + 1, j);
+                            if (((j - 1) >= 0) && (!opened[i, j - 1]))
+                                revealCells(i, j - 1);
+                            if (((j + 1) <= gridSize - 1) && (!opened[i, j + 1]))
+                                revealCells(i, j + 1);
+                        }
+                    }
         }
         private void overGame()
         {
+            gameOn = false;
             foreach (Button b in mineButtons)
             {
                 b.Background = new SolidColorBrush(Colors.Red);
@@ -134,13 +153,14 @@ namespace MineSweeper
         }
         private void winGame()
         {
+            gameOn = false;
             foreach (Button b in mineButtons)
             {
                 b.Background = new SolidColorBrush(Colors.Green);
                 b.Foreground = new SolidColorBrush(Colors.Black);
                 b.IsHitTestVisible = false;
             }
-            MessageBox.Show("Hurray ! you won.");
+            MessageBox.Show("Congrats ! you won in "+(minutes*60+seconds)+" seconds");
             backButton.Content = "Won!!! (Back For a new game)";
         }
 
@@ -172,7 +192,7 @@ namespace MineSweeper
                         {
                             for (int l = j - 1; l <= j + 1; l++)
                             {
-                                if ((k >= 0 && k < gridSize)&&(l >= 0 && l< gridSize))
+                                if ((k >= 0 && k < gridSize) && (l >= 0 && l < gridSize))
                                 {
                                     String s1 = (String)buttons[k, l].Content;
                                     if (s1.Equals("*"))
@@ -183,6 +203,32 @@ namespace MineSweeper
                     }
                 }
             }
+        }
+        public void manageTimer()
+        {
+            Boolean inGame = false ;
+            while (true)
+            {
+                while (gameOn)
+                {
+                    inGame = true;
+                    seconds++;
+                    if (seconds == 60) { minutes++; seconds = 0; }
+                    Deployment.Current.Dispatcher.BeginInvoke(() => { timerButton.Content = "" + minutes + ":" + seconds; });
+                    Thread.Sleep(1000);
+                }
+                if (inGame) break;
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            flagOn = true;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            flagOn = false;
         }
     }
 }
